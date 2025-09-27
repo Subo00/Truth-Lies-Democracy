68460d1498eb4c892b17fc033c7ca8c5967e5dc1
Rhinör <rainer.laschober@gmx.at>
2025-09-27 21:37:02 +0200

    Create assignment script (WIP)

---
+++ Assets/Scripts/Assignment.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+public class Assignment : MonoBehaviour
+{
+    public int Id;
+    public string MediaOutlet;
+    public string Headline;
+    public string Description;
+    public float Reward; // € or $
+    public int Ethics_On_Accept;
+    public int Ethics_On_Reject;
+    public float TimeRemaining;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
