49bfe3943f312ea2bd2f048c6c07f964ba520ea5
Ernest Subotić <gamedevsubo@gmail.com>
2025-09-27 13:41:33 +0200

    Update moving

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,37 +1,68 @@
 
 using UnityEngine;
 
+public enum Direction { Up, Down, Left, Right } 
+
 public class Mover : MonoBehaviour
 {
     [SerializeField] private float farRight;
     [SerializeField] private float farLeft;
+    [SerializeField] private float farUp;
+    [SerializeField] private float farDown;
     [SerializeField] private float speed = 5f;
     private float currentX = 0f;
+    private float currentY = 0f;
     private bool movingLeft = false;
     private bool movingRight = false;
-   
+    private bool movingUp = false;
+    private bool movingDown = false;
 
     // Update is called once per frame
     void Update()
     {
         currentX = transform.position.x;
+        currentY = transform.position.y;
         if(movingRight && currentX < farRight) { transform.position += new Vector3(1, 0, 0) * speed * Time.deltaTime; }
         if(movingLeft && currentX > farLeft) { transform.position += new Vector3(-1, 0, 0) * speed * Time.deltaTime; }
+        if(movingUp && currentY < farUp) { transform.position += new Vector3(0, 1, 0) * speed * Time.deltaTime; }
+        if(movingDown && currentY > farDown) { transform.position += new Vector3(0, -1, 0) * speed * Time.deltaTime; }
+    }
+    
+    public void Move(Direction dir)
+    {
+        switch (dir)
+        {
+            case Direction.Left:
+                movingLeft = true;
+                break;
+            case Direction.Right:
+                movingRight = true;
+                break;
+            case Direction.Up:
+                movingUp = true;
+                break;
+            case Direction.Down:
+                movingDown = true;
+                break;
+        }
     }
 
-    public void MoveLeft()
+    public void MoveClear(Direction dir)
     {
-        movingLeft = true;
-    }
-
-    public void MoveRight()
-    {
-        movingRight = true;
-    }
-
-    public void MoveClear()
-    {
-        movingLeft = false;
-        movingRight = false;
+        switch (dir)
+        {
+            case Direction.Left:
+                movingLeft = false;
+                break;
+            case Direction.Right:
+                movingRight = false;
+                break;
+            case Direction.Up:
+                movingUp = false;
+                break;
+            case Direction.Down:
+                movingDown = false;
+                break;
+        }
     }
 }
--- Assets/Scripts/OnMouseEnter.cs
+++ Assets/Scripts/OnMouseEnter.cs
@@ -4,18 +4,17 @@ using UnityEngine.EventSystems;
 public class OnMouseEnter : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private Mover mover;
-    [SerializeField] private bool isLeft;
+    [SerializeField] private Direction direction;
 
     //Detect if the Cursor starts to pass over the GameObject
     public void OnPointerEnter(PointerEventData pointerEventData)
     {
-        if (isLeft) { mover.MoveLeft(); }
-        else { mover.MoveRight(); }
+        mover.Move(direction);
     }
 
     //Detect when Cursor leaves the GameObject
     public void OnPointerExit(PointerEventData pointerEventData)
     {
-        mover.MoveClear();
+        mover.MoveClear(direction);
     }
 }
