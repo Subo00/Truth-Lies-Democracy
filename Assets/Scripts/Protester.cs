b31155a4743b3b2529a89e865f603dc98d83824a
Rhinör <rainer.laschober@gmx.at>
2025-09-27 16:35:19 +0200

    Implement logic for create sprites from properties for Protester and OldPerson

--- Assets/Scripts/Protester.cs
+++ Assets/Scripts/Protester.cs
@@ -3,28 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum BodyType
+public enum Body
 {
-    ROUND,
+    NEUTRAL,
     SLIM,
-    OLD,
 }
-
-public enum ClothingColor
-{
-    RED,
-    GREEN,
-    BLUE,
-}
-
 public enum Eyes
 {
     NEUTRAL,
-    ANGRY
+    ANGRY,
 }
-
-public enum Hair
+public enum HairType
 {
+    NONE,
     HAIR1,
     HAIR2,
     HAIR3,
@@ -37,32 +28,27 @@ public enum Hair
     HAIR10,
     HAIR11,
 }
-
-public enum HeadColor
+public enum Head
 {
-    WHITE,
-    BEIGE,
-    LIGHT_BROWN,
-    DARK_BROWN,
+    HEAD1,
+    HEAD2,
+    HEAD3,
+    HEAD4,
 }
-
 public enum Mouth
 {
     NEUTRAL,
     SHOUTING,
     SMILING,
 }
-
-public enum Arm
+public enum Attachment
 {
-    EMPTY,
+    NONE,
     SIGN,
     CHILD,
-    CANE,
-    WALKER,
 }
 
-public enum ChildHair
+public enum ChildHairType
 {
     NONE,
     HAIR1,
@@ -73,37 +59,158 @@ public enum ChildHair
     HAIR6,
     HAIR7,
 }
-public enum ChildHeadColor
+public enum ChildHead
 {
     NONE,
-    WHITE,
-    BEIGE,
-    LIGHT_BROWN,
-    DARK_BROWN,
+    HEAD1,
+    HEAD2,
+    HEAD3,
+    HEAD4,
 }
 
 public class Protester : MonoBehaviour
 {
-    public BodyType BodyType;
-    public ClothingColor ClothingColor;
+    public Body Body;
+    public Color BodyColor;
     public Eyes Eyes;
-    public Hair Hair;
-    public HeadColor HeadColor;
+    public HairType HairType;
+    public Color HairColor;
+    public Head Head;
     public Mouth Mouth;
-    public Arm Arm;
-    public ChildHair ChildHair;
-    public ChildHeadColor ChildHeadColor;
+    public Attachment Attachment;
 
+    public ChildHairType ChildHairType;
+    public Color? ChildHairColor;
+    public ChildHead ChildHead;
 
-    // Start is called before the first frame update
-    void Start()
+    public Protester(
+        Body body,
+        Color bodyColor,
+        Eyes eyes,
+        HairType hairType,
+        Color hairColor,
+        Head head,
+        Mouth mouth,
+        Attachment attachment,
+
+        ChildHairType childHairType = ChildHairType.NONE,
+        Color? childHairColor = null,
+        ChildHead childHead = ChildHead.NONE)
     {
-        
+        Body = body;
+        BodyColor = bodyColor;
+        Eyes = eyes;
+        HairType = hairType;
+        HairColor = hairColor;
+        Head = head;
+        Mouth = mouth;
+        Attachment = attachment;
+
+        ChildHairType = childHairType;
+        ChildHairColor = childHairColor;
+        ChildHead = childHead;
     }
 
-    // Update is called once per frame
-    void Update()
+    public SpriteRenderer BodySprite = new SpriteRenderer();
+    public SpriteRenderer EyesSprite = new SpriteRenderer();
+    public SpriteRenderer HairSprite = new SpriteRenderer();
+    public SpriteRenderer HeadSprite = new SpriteRenderer();
+    public SpriteRenderer MouthSprite = new SpriteRenderer();
+    public SpriteRenderer ArmSprite = new SpriteRenderer();
+    public SpriteRenderer AttachmentSprite = new SpriteRenderer();
+
+    public SpriteRenderer ChildHairSprite = new SpriteRenderer();
+    public SpriteRenderer ChildHeadSprite = new SpriteRenderer();
+    public SpriteRenderer ChildFaceSprite = new SpriteRenderer();
+
+    void Start()
     {
-        
+        // Body
+        BodySprite.sprite = Body switch
+        {
+            Body.SLIM => Resources.Load("Images/Protester/body/bodySlim") as Sprite,
+            _ => Resources.Load("Images/Protester/body/body") as Sprite,
+        };
+        BodySprite.color = BodyColor;
+
+        // Eyes
+        BodySprite.sprite = Eyes switch
+        {
+            Eyes.ANGRY => Resources.Load("Images/Protester/head/eyes/eyesAngry") as Sprite,
+            _ => Resources.Load("Images/Protester/head/eyes/eyes1") as Sprite,
+        };
+
+        // Hair
+        HairSprite.sprite = HairType switch
+        {
+            HairType.HAIR1 => Resources.Load("Images/Protester/head/hair/hair1") as Sprite,
+            HairType.HAIR2 => Resources.Load("Images/Protester/head/hair/hair2") as Sprite,
+            HairType.HAIR3 => Resources.Load("Images/Protester/head/hair/hair3") as Sprite,
+            HairType.HAIR4 => Resources.Load("Images/Protester/head/hair/hair4") as Sprite,
+            HairType.HAIR5 => Resources.Load("Images/Protester/head/hair/hair5") as Sprite,
+            HairType.HAIR6 => Resources.Load("Images/Protester/head/hair/hair6") as Sprite,
+            HairType.HAIR7 => Resources.Load("Images/Protester/head/hair/hair7") as Sprite,
+            HairType.HAIR8 => Resources.Load("Images/Protester/head/hair/hair8") as Sprite,
+            HairType.HAIR9 => Resources.Load("Images/Protester/head/hair/hair9") as Sprite,
+            HairType.HAIR10 => Resources.Load("Images/Protester/head/hair/hair10") as Sprite,
+            HairType.HAIR11 => Resources.Load("Images/Protester/head/hair/hair11") as Sprite,
+            _ => null,
+        };
+        HairSprite.color = HairColor;
+
+        // Head
+        HeadSprite.sprite = Head switch
+        {
+            Head.HEAD2 => Resources.Load("Images/Protester/head/Head/head2") as Sprite,
+            Head.HEAD3 => Resources.Load("Images/Protester/head/Head/head3") as Sprite,
+            Head.HEAD4 => Resources.Load("Images/Protester/head/Head/head4") as Sprite,
+            _ => Resources.Load("Images/Protester/head/Head/head1") as Sprite,
+        };
+
+        // Mouth
+        MouthSprite.sprite = Mouth switch
+        {
+            Mouth.SHOUTING => Resources.Load("Images/Protester/head/mouth/mouthShouting") as Sprite,
+            Mouth.SMILING=> Resources.Load("Images/Protester/head/mouth/mouthSmiling") as Sprite,
+            _ => Resources.Load("Images/Protester/head/mouth/mouthNeutral") as Sprite,
+        };
+
+        // Arm + Attachment
+        if (Attachment == Attachment.SIGN)
+        {
+            ArmSprite.sprite = Resources.Load("Images/Protester/sign/signArm") as Sprite;
+            AttachmentSprite.sprite = Resources.Load("Images/Protester/sign/sign") as Sprite;
+        }
+        else if (Attachment == Attachment.CHILD)
+        {
+            ArmSprite.sprite = Resources.Load("Images/Protester/Child/adultArm") as Sprite;
+            AttachmentSprite.sprite = Resources.Load("Images/Protester/Child/body") as Sprite;
+
+            // Child Hair
+            ChildHairSprite.sprite = ChildHairType switch
+            {
+                ChildHairType.HAIR1 => Resources.Load("Images/Protester/Child/Childhead/hair/hair1") as Sprite,
+                ChildHairType.HAIR2 => Resources.Load("Images/Protester/Child/Childhead/hair/hair2") as Sprite,
+                ChildHairType.HAIR3 => Resources.Load("Images/Protester/Child/Childhead/hair/hair3") as Sprite,
+                ChildHairType.HAIR4 => Resources.Load("Images/Protester/Child/Childhead/hair/hair4") as Sprite,
+                ChildHairType.HAIR5 => Resources.Load("Images/Protester/Child/Childhead/hair/hair5") as Sprite,
+                ChildHairType.HAIR6 => Resources.Load("Images/Protester/Child/Childhead/hair/hair6") as Sprite,
+                ChildHairType.HAIR7 => Resources.Load("Images/Protester/Child/Childhead/hair/hair7") as Sprite,
+                _ => null,
+            };
+            ChildHairSprite.color = (Color) ChildHairColor;
+
+            // Child Head
+            ChildHeadSprite.sprite = ChildHead switch
+            {
+                ChildHead.HEAD2 => Resources.Load("Images/Protester/Child/Childhead/head/head2") as Sprite,
+                ChildHead.HEAD3 => Resources.Load("Images/Protester/Child/Childhead/head/head3") as Sprite,
+                ChildHead.HEAD4 => Resources.Load("Images/Protester/Child/Childhead/head/head4") as Sprite,
+                _ => Resources.Load("Images/Protester/Child/Childhead/head/head1") as Sprite,
+            };
+
+            // Child Face
+            ChildFaceSprite.sprite = Resources.Load("Images/Protester/Child/Childhead/face") as Sprite;
+        }
     }
 }
